bc833bdb664844b9863857100e883eed51ca9af1
austin <ap4000@nyu.edu>
2017-04-13 00:51:21 -0400

    successful-ish Ch.7 tests

--- projects/07/translator.c
+++ projects/07/translator.c
@@ -4,13 +4,15 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <unistd.h>
 
 char *get_filename_ext(char*);
 void parsefile(char*,char*);
-void parseline(char*,char*);
+void parseline(char*,char*,int*);
 int get_next_word(char*,char*,int*);
-void parsearith(char*,char*);
+void parsearith(char*,char*,int*);
 void parsepush(char*,char*,char*);
+char *ident(char*,int*);
 
 int main(int argc, char** argv) {
 
@@ -32,7 +34,7 @@ int main(int argc, char** argv) {
     }
     strcat(OUT,"(END)\n@END\n0;JMP");
     OUT[strlen(OUT)] = '\0';
-    FILE *fwrite = fopen("Test.asm","w");
+    FILE *fwrite = fopen(argv[2],"w");
     fputs(OUT,fwrite);
     printf("%s\n",OUT);
     free(OUT);
@@ -49,27 +51,32 @@ char *get_filename_ext(char *filename) {
 void parsefile(char* rfile,char* out) {
     FILE *rf = fopen(rfile,"r");
     char str[100];
+    int jj[1];
+    jj[0] = 0;
+    printf("%i\n",jj[0]);
     int i;
     while (fgets(str,100,rf) != NULL) {
-        parseline(str,out);
+        parseline(str,out,jj);
     }
 }
 
-void parseline(char* line, char* out) {
-    if ((line[0] == '/' && line[1] == '/') || line[0] == '\n' || line[0] == ' ' || line[0] == '\0' || line[0] == 13) {
+void parseline(char* line, char* out,int* jj) {
+    if ((line[0] == '/' && line[1] == '/') || line[0] == '\n' || line[0] == ' ' || line[0] == '\0' || line[0] <= 13) {
         return;
     }
-    int j = 0;
-    int *i = &j;
-    char *temp = (char*) malloc(strlen(line)*sizeof(char));
+    int i[0];
+    *i = 0;
+    jj[0] += 1;
+    printf("%i\n",*jj);
+    char *temp = (char*) malloc((strlen(line)+1)*sizeof(char));
     get_next_word(line,temp,i);
-    char *command = (char*) malloc(strlen(temp)*sizeof(char));
+    char *command = (char*) malloc((strlen(temp)+1)*sizeof(char));
     strcpy(command,temp);
     if (strcmp(command,"push") != 0 &&  strcmp(command,"pop") != 0){
-        parsearith(command,out);
+        parsearith(command,out,jj);
     } else {
         get_next_word(line,temp,i);
-        char * segment = (char*) malloc(strlen(temp)*sizeof(char));
+        char * segment = (char*) malloc((strlen(temp)+1)*sizeof(char));
         strcpy(segment,temp);
         get_next_word(line,temp,i);
         char *index = temp;
@@ -85,7 +92,7 @@ void parseline(char* line, char* out) {
 
 int get_next_word(char* line,char *buff,int* start) {
     int b = 0;
-    while (line[*start] != ' ' && line[*start] != '\0' && line[*start] != '\n' && line[*start] != 13) {
+    while (line[*start] != ' ' && line[*start] != '\0' && line[*start] != '\n' && line[*start] > 13 && line[*start] != 133) {
         buff[b] = line[*start];
         (*start)++;
         b++;
@@ -95,27 +102,81 @@ int get_next_word(char* line,char *buff,int* start) {
     return *start;
 }
 
-void parsearith(char *oper, char *out) {
-    char *str = (char*) malloc(100*sizeof(char));
+void parsearith(char *oper, char *out,int* jumpc) {
+    char *str = (char*) malloc(1000*sizeof(char));
+    str[0] = '\0';
     strcat(str,"@SP\nM=M-1\nA=M\nD=M\n@SP\n");
+
     if (strcmp(oper, "add") == 0){
         strcat(str,"M=M-1\nA=M\nD=M+D\n");
     } else if (strcmp(oper, "sub") == 0)
         strcat(str,"M=M-1\nA=M\nD=M-D\n");
     else if (strcmp(oper, "neg") == 0)
-        strcat(str,"D=-D\nM=D");
-    else if (strcmp(oper, "eq") == 0)
-        strcat(str,"M=M-1\nA=M\nD=D-M\nD;JEQ\n");
-    else if (strcmp(oper, "gt") == 0)
-        strcat(str,"\n");
-    else if (strcmp(oper, "lt") == 0)
-         strcat(str,"\n");
+        strcat(str,"D=-D\n");
     else if (strcmp(oper, "and") == 0)
-        strcat(str,"\n");
+        strcat(str,"M=M-1\nA=M\nD=D&M\n");
     else if (strcmp(oper, "or") == 0)
-        strcat(str,"\n");
-    else
-        strcat(str,"A=!A\nM=M+1\n");
+        strcat(str,"M=M-1\nA=M\nD=D|M\n");
+    else if (strcmp(oper,"not") == 0)
+        strcat(str,"D=!D\n"); else {
+        strcat(str,"D=D\n@START");
+        //printf("%i\n",*jumpc);
+        char * id = (char*) malloc(4*sizeof(char));
+        ident(id,jumpc);
+        char *tr = (char*) malloc(25*sizeof(char));
+        char *fa = (char*) malloc(25*sizeof(char));
+        int k;
+        tr[0] = '\0';
+        fa[0] = '\0';
+
+        strcat(str,id);
+        strcat(str,"\n0;JMP\n");
+
+        strcat(tr,"(TRUE");
+        strcat(tr,id);
+        strcat(tr,")\n");
+
+        strcat(fa,"(FALSE");
+        strcat(fa,id);
+        strcat(fa,")\n");
+
+        printf("%s%s\n",tr,fa);
+
+
+        strcat(str,tr);
+        strcat(str,"D=-1\n@CONT");
+        strcat(str,id);
+        strcat(str,"\n0;JMP\n");
+        strcat(str,fa);
+        strcat(str,"D=0\n@CONT");
+        strcat(str,id);
+        strcat(str,"\n0;JMP\n");
+
+        strcat(str,"(START");
+        strcat(str,id);
+        strcat(str,")\n");
+
+        strcat(str,"@SP\nM=M-1\nA=M\nD=M-D\n@TRUE");
+        strcat(str,id);
+        strcat(str,"\nD;");
+
+        if (strcmp(oper,"eq") == 0) 
+            strcat(str,"JEQ");
+        else if (strcmp(oper,"gt") == 0)
+            strcat(str,"JGT");
+        else if (strcmp(oper,"lt") == 0)
+            strcat(str,"JLT");
+        strcat(str,"\n@FALSE");
+        strcat(str,id);
+        strcat(str,"\n0;JMP\n(CONT");
+        strcat(str,id);
+        strcat(str,")\n");
+
+        free(id);
+        free(tr);
+        free(fa);
+
+    }
     strcat(str,"@SP\nA=M\nM=D\n@SP\nM=M+1\n");
     strcat(out,str);
     free(str);
@@ -134,4 +195,17 @@ void parsepush(char *segment, char *index,char *out) {
     }
     strcat(out,str);
     free(str);
+}
+
+char * ident(char* buff,int *i) {
+    int temp = *i;
+    int j;
+    for (j=0;j<3;j++) {
+        if (temp > 0) {
+            buff[j] = (temp % 10) + '0';
+            temp /= 10;
+        } else
+            buff[j] = 'z';
+    }
+    buff[3] = '\0';
 }
